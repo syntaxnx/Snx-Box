6948c558d53a332ebb2b76bdf361dc0f01338b18
syntaxnx <120047630+syntaxnx@users.noreply.github.com>
2022-12-07 17:24:33 -0300

    Add files via upload

---
+++ snx.sh
@@ -0,0 +1,136 @@
+#!/data/data/com.termux/files/usr/bash
+echo $red
+echo "------------------------------ "
+echo
+echo "  _______  _______  ___ ___ ";
+echo " |     __||    |  ||   |   | ";
+echo " |__     ||       ||-     -| ";
+echo " |_______||__|____||___|___| ";
+echo 
+echo "------------------------------ "
+echo
+echo " Instalador de pacotes "  
+echo " powered by syntaxx "
+echo " versão 4.0.0"    
+echo                                                   
+
+echo
+echo
+echo
+echo
+
+echo "."
+
+apt update -y
+apt upgrade -y       #ATUALIZAÇÃO EM ANDAMENTO
+apt install python -y   #INSTALAÇÃ INICIADA !
+apt install python2 -y
+apt install python-dev -y
+apt install ncurses -y
+apt install python3 -y
+apt install nano -y
+apt install host -y
+apt install docker -y
+apt install ruby -y 
+apt install php7 -y
+apt install ftp -y
+apt install php-apache -y
+apt install netcat -y
+apt install binutils-is-llvm -y
+apt install binutils -y
+apt install motd -y
+apt install libfreetype6 -y
+apt install libcups2 -y
+apt install libxext -y
+apt install libxrender -y
+apt install libasound2 -y
+apt install libffi -y
+apt install autoconf -y
+apt install cowsay-y
+apt install dnsutils -y 
+apt install coreutils -y
+apt install bat -y
+apt install openssl -y
+apt install fish -y
+apt install tar -y 
+apt install sed -y
+apt install readline
+apt install psmisc -y
+apt install macchanger -y
+apt install java-y
+apt install zlib -y
+apt install xz-utils -y
+apt install xxhash -y
+apt install util-linux -y
+apt install unbound -y
+apt install termux-tools -y
+apt install termux-am -y
+apt install termux-exec -y
+apt install termux-am-socket -y
+apt install host -y
+apt install wcalc -y
+apt install proot -y
+apt install golang -y
+apt install figlet -y
+apt install w3m -y
+apt install vim -y
+apt install git -y
+apt install php -y 
+apt install docker -y
+apt install perl -y
+apt install nmap -y
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install macchanger -y
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install x11-rep -y
+apt install figlet toilet -y
+apt install lolcat -y
+apt install nodejs -y
+apt install nodejs-lts -y
+apt install openjdk-17 -y
+apt install ffmpeg -y
+apt install tesseract -y
+apt install zip -y
+apt install unzip -y
+apt install tor -y
+apt install wget -y
+apt install wcalc -y 
+apt install openssl -y
+apt install ssh -y
+apt install loclx -y
+gem install wpscan -y
+apt install root-repo -y
+apt install rust -y
+apt install curl gnupg -y
+apt install bmon -y
+apt install apache2 -y
+pip install --upgrade -y
+pip install lolcat -y
+pip install requests -y
+pkg upgrade -y && pkg update -y && pkg install nodejs -y && pkg install nodejs-lts -y && pkg install ffmpeg -y && pkg install wget -y && pkg install git -y
+echo
+echo  "--------------------------------"
+echo  "   INSTALAÇÃO DE FERRAMENTAS "
+echo  "--------------------------------"
+
+echo git 
+git clone https://github.com/ekadanuarta/Tool-X.git
+git clone https://github.com/OnlineHacKing/CameraHack.git
+git clone https://github.com/p4rad0x-px/Termux-Mod
+
+echo
+echo 
+
+echo "   ➜ INSTALAÇÃO CONCLUÍDA "
+echo
+echo  "--------------------------------"
+echo          "  ~ BY SYNTAX "               
+echo       " github.com/syntaxnx"             
+echo  "--------------------------------"
+echo
+echo
+exit
